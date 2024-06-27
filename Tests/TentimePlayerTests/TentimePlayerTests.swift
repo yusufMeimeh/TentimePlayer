2b9a175405d99303199d20bdf53f877fd54cacb5
Qamar Al Amassi <qamar.alamassi@tentimejo.com>
2024-06-27 14:01:43 +0300

    Initial Commit

---
+++ Tests/TentimePlayerTests/TentimePlayerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import TentimePlayer
+
+final class TentimePlayerTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
